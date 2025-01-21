9dc975571d6b97b71b203e49433c73a34755194a
Guillermo Bustamante <64450610+guillebusta10@users.noreply.github.com>
2025-01-21 18:43:55 +0000

    leertxt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 using namespace std;
+class Nodo{
+    public:
+        char letra;
+        int distancia;
+        vector<Nodo*> hijos;
+    Nodo(char _letra, int _distancia){
+        letra=_letra;
+        distancia=_distancia;
 
+    }
+};
+vector<vector<int>> leerTxT(string Archivo, int nodos){
+    ifstream file(Archivo);
+    file>>nodos;
+
+    vector<vector<int>>matriz(nodos,vector<int>(nodos));
+    for(int i=0;i<nodos;i++){
+        for(int j=0;j<nodos;j++){
+            file>>matriz[i][j];
+            if(file.peek()==','){
+                file.ignore();
+            }
+        }
+    }
+    
+    return matriz;
+
+
+
+
+
+
+}
 int main(){
-    cout<<"hola mundo"<<endl;
+    int nodos;
+    vector<vector<int>> matriz=leerTxT("matriz.txt",nodos);
+
     return 0;
 }
